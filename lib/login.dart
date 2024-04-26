4adc32654c6693c2a1afe7482151557abee4d88d
Yorqinbek <yyoriqulov@mail.ru>
2024-04-26 22:57:24 +0500

    Login otp

--- lib/login.dart
+++ lib/login.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:soqchi/childs_list.dart';
+import 'package:soqchi/login_otp.dart';
 import 'package:soqchi/poster_help/post_helper.dart';
 import 'dart:convert';
 
@@ -16,6 +18,12 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   ValueNotifier userCredential = ValueNotifier('');
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  bool isvalidate = false;
+
+  bool next_btn = false;
+
+  String phone_number = "";
 
   Future<dynamic> signInWithGoogle() async {
     try {
@@ -45,9 +53,51 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+  Future<void> send_phone(String phone_number) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LoginOtpPage(
+        phone_number: phone_number,
+      );
+    }));
+
+    // userCredential.value = await signInWithGoogle();
+    // if (userCredential.value != null) {
+    //   // print(userCredential.value);
+    //   var user_email = userCredential.value.user!.email;
+    //   var name = userCredential.value.user!.displayName;
+    //   var user_id = userCredential.value.user!.uid;
+    //   Map data = {'email': user_email, 'password': user_id, 'name': name};
+    //   var response = await post_helper(data, '/register');
+    //   if (response != "Error") {
+    //     var data = jsonDecode(response);
+    //     print(data);
+    //     // final Map parsed = json.decode(response);
+
+    //     if (data['status'] ||
+    //         data['message']
+    //             .toString()
+    //             .contains('The email has already been taken.')) {
+    //       final SharedPreferences prefs = await _prefs;
+    //       prefs.setBool("regstatus", true);
+    //       prefs.setString("email", user_email);
+    //       prefs.setString("uid", user_id);
+    //       Navigator.pushReplacement(context,
+    //           MaterialPageRoute(builder: (context) {
+    //         return ChildListPage();
+    //       }));
+    //     }
+    //   }
+    // }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final TextEditingController controller = TextEditingController();
+    String initialCountry = 'UZ';
+    PhoneNumber number = PhoneNumber(isoCode: 'UZ');
+
     return Scaffold(
+        backgroundColor: Colors.grey[200],
         body: SafeArea(
             child: ValueListenableBuilder(
                 valueListenable: userCredential,
@@ -57,95 +107,141 @@ class _LoginPageState extends State<LoginPage> {
                       ? Padding(
                           padding: const EdgeInsets.all(20.0),
                           child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(
-                                "Sign up",
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.05,
-                              ),
-                              Text(
-                                "Lorem ipsum doler set amet lorem ipsum doler set amet",
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 20,
-                                ),
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.25,
-                              ),
-                              Container(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.6,
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.07,
-                                  child: ElevatedButton(
-                                    style: ButtonStyle(
-                                      backgroundColor:
-                                          MaterialStateProperty.all(
-                                              Colors.black),
-                                      shape: MaterialStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(
-                                              20), // radius of the corners
+                              Column(
+                                children: [
+                                  SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.05,
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Telefon raqamingizni kiriting",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 28,
                                         ),
                                       ),
-                                    ),
-                                    onPressed: () async {
-                                      // var a = await signInWithGoogle();
-                                      userCredential.value =
-                                          await signInWithGoogle();
-                                      if (userCredential.value != null) {
-                                        // print(userCredential.value);
-                                        var user_email =
-                                            userCredential.value.user!.email;
-                                        var name = userCredential
-                                            .value.user!.displayName;
-                                        var user_id =
-                                            userCredential.value.user!.uid;
-                                        Map data = {
-                                          'email': user_email,
-                                          'password': user_id,
-                                          'name': name
-                                        };
-                                        var response = await post_helper(
-                                            data, '/register');
-                                        if (response != "Error") {
-                                          var data = jsonDecode(response);
-                                          print(data);
-                                          // final Map parsed = json.decode(response);
-
-                                          if (data['status'] ||
-                                              data['message'].toString().contains(
-                                                  'The email has already been taken.')) {
-                                            final SharedPreferences prefs =
-                                                await _prefs;
-                                            prefs.setBool("regstatus", true);
-                                            prefs.setString(
-                                                "email", user_email);
-                                            prefs.setString("uid", user_id);
-                                            Navigator.pushReplacement(context,
-                                                MaterialPageRoute(
-                                                    builder: (context) {
-                                              return ChildListPage();
-                                            }));
-                                          }
-                                        }
+                                      SizedBox(
+                                        height: 15,
+                                      ),
+                                      Text(
+                                        "Bu tizimga kirishingiz uchun zarur",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.1,
+                                  ),
+                                  InternationalPhoneNumberInput(
+                                    countries: ["UZ"],
+                                    autoFocus: true,
+                                    onInputChanged: (PhoneNumber number) {
+                                      print(number.phoneNumber);
+                                      print(number.phoneNumber!.length);
+                                      print(isvalidate);
+                                      if (number.phoneNumber!.length == 13 &&
+                                          isvalidate) {
+                                        setState(() {
+                                          phone_number = number.phoneNumber!;
+                                          next_btn = true;
+                                        });
+                                      } else {
+                                        setState(() {
+                                          next_btn = false;
+                                        });
                                       }
                                     },
-                                    child: Text(
-                                      "Google",
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 18),
+                                    onInputValidated: (bool value) {
+                                      print(value);
+                                      setState(() {
+                                        isvalidate = true;
+                                      });
+                                    },
+                                    selectorConfig: SelectorConfig(
+                                      selectorType:
+                                          PhoneInputSelectorType.BOTTOM_SHEET,
                                     ),
-                                  )),
+                                    hintText: "Telefon raqamingiz",
+                                    ignoreBlank: false,
+                                    autoValidateMode: AutovalidateMode.disabled,
+                                    selectorTextStyle: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                    textStyle: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                    initialValue: number,
+                                    textFieldController: controller,
+                                    formatInput: true,
+                                    keyboardType:
+                                        TextInputType.numberWithOptions(
+                                            signed: true, decimal: true),
+                                    // inputBorder: OutlineInputBorder(),
+                                    inputBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(12),
+                                        borderSide: BorderSide.none),
+
+                                    onSaved: (PhoneNumber number) {
+                                      // number.toString()
+                                      // print(number.toString());
+                                      // print('On Saved: $number');
+                                    },
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    "Davom etish tugmasini bosish orqali siz ommaviy oferta shartlariga roziligingizni bildirasiz",
+                                    style: TextStyle(
+                                        color: Colors.grey, fontSize: 12),
+                                  ),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.9,
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.07,
+                                      child: ElevatedButton(
+                                        style: ButtonStyle(
+                                          backgroundColor: next_btn == true
+                                              ? MaterialStateProperty.all(
+                                                  Colors.blueAccent)
+                                              : MaterialStateProperty.all(
+                                                  Colors.grey),
+                                          shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                            RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(
+                                                  15), // radius of the corners
+                                            ),
+                                          ),
+                                        ),
+                                        onPressed: next_btn == true
+                                            ? () => send_phone(phone_number)
+                                            : () {},
+                                        child: Text(
+                                          "Davom etish",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18),
+                                        ),
+                                      )),
+                                ],
+                              ),
                             ],
                           ),
                         )
---
+++ lib/login_otp.dart
@@ -0,0 +1,244 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+import 'package:flutter_timer_countdown/flutter_timer_countdown.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:soqchi/childs_list.dart';
+import 'package:soqchi/poster_help/post_helper.dart';
+import 'dart:convert';
+
+class LoginOtpPage extends StatefulWidget {
+  final String phone_number;
+  const LoginOtpPage({super.key, required this.phone_number});
+
+  @override
+  State<LoginOtpPage> createState() => _LoginOtpPageState();
+}
+
+class _LoginOtpPageState extends State<LoginOtpPage> {
+  ValueNotifier userCredential = ValueNotifier('');
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  Future<dynamic> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } on Exception catch (e) {
+      // TODO
+      print('exception->$e');
+    }
+  }
+
+  Future<bool> signOutFromGoogle() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      return true;
+    } on Exception catch (_) {
+      return false;
+    }
+  }
+
+  Future<void> send_phone() async {
+    userCredential.value = await signInWithGoogle();
+    if (userCredential.value != null) {
+      // print(userCredential.value);
+      var user_email = userCredential.value.user!.email;
+      var name = userCredential.value.user!.displayName;
+      var user_id = userCredential.value.user!.uid;
+      Map data = {'email': user_email, 'password': user_id, 'name': name};
+      var response = await post_helper(data, '/register');
+      if (response != "Error") {
+        var data = jsonDecode(response);
+        print(data);
+        // final Map parsed = json.decode(response);
+
+        if (data['status'] ||
+            data['message']
+                .toString()
+                .contains('The email has already been taken.')) {
+          final SharedPreferences prefs = await _prefs;
+          prefs.setBool("regstatus", true);
+          prefs.setString("email", user_email);
+          prefs.setString("uid", user_id);
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) {
+            return ChildListPage();
+          }));
+        }
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController controller = TextEditingController();
+    String initialCountry = 'UZ';
+    PhoneNumber number = PhoneNumber(isoCode: 'UZ');
+
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey[200],
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        backgroundColor: Colors.grey[200],
+        body: SafeArea(
+            child: ValueListenableBuilder(
+                valueListenable: userCredential,
+                builder: (context, value, child) {
+                  return (userCredential.value == '' ||
+                          userCredential.value == null)
+                      ? Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Sms kodni kiriting",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 28,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 15,
+                                      ),
+                                      Text(
+                                        widget.phone_number.substring(
+                                                0,
+                                                widget.phone_number.length -
+                                                    4) +
+                                            ' ** **' +
+                                            " raqamga kod yuborildi",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.12,
+                                  ),
+                                  OtpTextField(
+                                    textStyle: TextStyle(fontSize: 25),
+                                    autoFocus: true,
+                                    numberOfFields: 5,
+                                    borderWidth: 4,
+                                    enabledBorderColor: Colors.grey,
+                                    borderColor: Colors.blue,
+                                    onCodeChanged: (String code) {
+                                      //handle validation or checks here
+                                    },
+                                    //runs when every textfield is filled
+                                    onSubmit: (String verificationCode) {
+                                      showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title: Text("Verification Code"),
+                                              content: Text(
+                                                  'Code entered is $verificationCode'),
+                                            );
+                                          });
+                                    }, // end onSubmit
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      TimerCountdown(
+                                        timeTextStyle: TextStyle(
+                                            color: Colors.grey, fontSize: 16),
+                                        enableDescriptions: false,
+                                        spacerWidth: 2,
+                                        format:
+                                            CountDownTimerFormat.minutesSeconds,
+                                        endTime: DateTime.now().add(
+                                          Duration(
+                                            minutes: 3,
+                                            seconds: 0,
+                                          ),
+                                        ),
+                                        onEnd: () {
+                                          print("Timer finished");
+                                        },
+                                      ),
+                                      Text(
+                                        "  dan so'ng qayta yuborish",
+                                        style: TextStyle(
+                                            color: Colors.grey, fontSize: 16),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.9,
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.07,
+                                      child: ElevatedButton(
+                                        style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all(
+                                                  Colors.blueAccent),
+                                          shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                            RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(
+                                                  15), // radius of the corners
+                                            ),
+                                          ),
+                                        ),
+                                        onPressed: send_phone,
+                                        child: Text(
+                                          "Davom etish",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18),
+                                        ),
+                                      )),
+                                ],
+                              ),
+                            ],
+                          ),
+                        )
+                      : Container(
+                          child: Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                        );
+                })));
+  }
+}
